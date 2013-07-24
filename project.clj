15403db2ccd1925c8c42344529c29fceb5642263
supersymmetry <maildumpert@gmail.com>
2013-07-24 20:27:05 +0200

    updated some references added guava

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Sample Code for Day of Datomic Presentation"
   :plugins [[lein-tg "0.0.1"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/test.generative "0.3.0"]
-                 [com.datomic/datomic-free "0.8.4020.24"]
-                 [incanter/incanter-charts "1.3.0"]
-                 [incanter/incanter-pdf "1.3.0"]])
+                 [clojure-guava "0.0.8"]
+                 [org.clojure/test.generative "0.4.0"]
+                 [com.datomic/datomic-free "0.8.4020"]
+                 [incanter/incanter-charts "1.5.1"]
+                 [incanter/incanter-pdf "1.5.1"]])
